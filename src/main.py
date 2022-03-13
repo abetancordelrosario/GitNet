0b56e98f753ecec77d4724335b96c1f9f0583335
alebdr <alebdr@protonmail.com>
2022-03-13 18:33:43 +0000

    feat: avances cración grafo

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from models.createGraph import createGraph
 from numpy import ndarray
+import time
 
 
 def main() -> None:   
-    graph = createGraph("alexisrolland/flask-graphene-sqlalchemy","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")
+    #graph = createGraph("alexisrolland/flask-graphene-sqlalchemy","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")
+    graph = createGraph("marius92mc/github-stargazers","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")
+    #graph = createGraph("jmportilla/Python-for-Algorithms--Data-Structures--and-Interviews","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")
+    initialTime = time.time()
     graph.add_vertices_and_edges()
+    print("Tiempo que tarda en ejecutar el programa:", (time.time() - initialTime), "seconds")
+    print(graph.g.num_vertices)
 
 if __name__ == "__main__":
     main()
--- src/models/createGraph.py
+++ src/models/createGraph.py
@@ -1,5 +1,5 @@
-import time
 from typing import List
+from warnings import catch_warnings
 from xmlrpc.client import boolean
 from github import Github, GithubException
 from graph_tool.all import *
@@ -7,13 +7,13 @@ from graph_tool.all import *
 class createGraph:
     """
     Creates a interest graph that includes the repository selected, the stargazers
-    of this repository and the stars of each stargazer.
+    of this repository and the starred repositories of each stargazer.
 
-    The construtor requires a two strings one the first is the full repository name 
-    (author/repository) and the second one is the user's token.
+    The construtor requires two strings, first is the full repository name 
+    (author/repository) and the second one is the user's GitHub token.
     """
 
-    __MAX_REPOS_STARGAZER: int = 500
+    __MAX_REPOS_STARGAZER: int = 20
     __IS_USER: boolean = True
     __IS_REPOSITORY: boolean = False
 
@@ -24,30 +24,50 @@ class createGraph:
         self.__v_name: VertexPropertyMap = self.g.new_vertex_property("string")
         self.__v_is_user: VertexPropertyMap = self.g.new_vertex_property("bool") 
         self.__e_relation: EdgePropertyMap = self.g.new_edge_property("string")
-
     
     def add_vertices_and_edges(self) -> None:
         self.create_vertex(self.__repository.name, self.__IS_REPOSITORY)
         main_vertex: Vertex = self.g.vertex(0)
-
-        stargazers: List = self.__repository.get_stargazers()
-        for index, stargazer in enumerate(stargazers):
-            self.create_vertex(stargazer.name, self.__IS_USER)
-            self.create_edge("stars", index, main_vertex)   
         
+        stargazers: List = [s for s in self.__repository.get_stargazers()]
+        for stargazer in stargazers:
+            new_stargazer_vertex: Vertex = self.create_vertex(stargazer.login, self.__IS_USER)
+            self.create_edge("starred", new_stargazer_vertex, main_vertex)
 
+        
+            for follower in stargazer.get_followers():
+                try:
+                    index =  stargazers.index(follower) 
+                    print(index)
+                except:
+                    pass
 
-    def create_vertex(self, name: str, type: boolean) -> None:
+            for starred in stargazer.get_starred()[:self.__MAX_REPOS_STARGAZER]:
+                repeated_repos: List = graph_tool.util.find_vertex(self.g, self.__v_name, starred.name)
+                if len(repeated_repos) != 0:         
+                    self.create_edge("starred", new_stargazer_vertex, repeated_repos[0])
+                else:  
+                    starred_repo: Vertex = self.create_vertex(starred.name, self.__IS_REPOSITORY)
+                    self.create_edge("starred", new_stargazer_vertex, starred_repo) 
+
+    def create_vertex(self, name: str, type: boolean) -> Vertex:
         vertex = self.g.add_vertex()
         self.__v_name[vertex] = name
         self.__v_is_user[vertex] = type
+        return vertex
 
-    def create_edge(self, relation: str, index: int, main_vertex: Vertex) -> None:
-        actual_vertex: Vertex = self.g.vertex(index + 1)
+    def create_edge(self, relation: str, actual_vertex: Vertex, main_vertex: Vertex) -> None:
         actual_edge: Edge = self.g.add_edge(actual_vertex,main_vertex)
         self.__e_relation[actual_edge] = relation
 
 
+
+
+
+
+
+
+      
 
 
 
