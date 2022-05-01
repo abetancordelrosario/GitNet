6341612577b6e2e895b8413b06b0129948309710
alebdr <alebdr@protonmail.com>
2022-05-01 18:36:02 +0100

    Deleted unused imports

--- src/main.py
+++ src/main.py
@@ -1,11 +1,4 @@
 from command import Command
-from models.dataExtraction import dataExtraction
-from models.draw import draw
-from models.interestGraph import interestGraph
-from models.dataProcessing import dataProcessing  
-import time
-
-
 
 def main() -> None:
     cli = Command()
